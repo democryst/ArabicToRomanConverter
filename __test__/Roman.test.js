2a90dc3a641f7186265462b3c693a55ed6720349
Pawit Manussirivitaya <democryst@gmail.com>
2017-07-19 16:14:08 +0700

    change Array's value

--- __test__/Roman.test.js
+++ __test__/Roman.test.js
@@ -4,9 +4,14 @@ test("given 1 should return I", function(){
     expect(result).toBe("I");
 })
 
-test("given 2 should return II", function(){
+xtest("given 2 should return II", function(){
     let result = roman(2);
     expect(result).toBe("II");
+})
+
+test("given 4 should return IV", function(){
+    let result = roman(4);
+    expect(result).toBe("IV");
 })
 
 test("given 5 should return V", function(){
@@ -14,14 +19,29 @@ test("given 5 should return V", function(){
     expect(result).toBe("V");
 })
 
+test("given 9 should return IX", function(){
+    let result = roman(9);
+    expect(result).toBe("IX");
+})
+
 test("given 10 should return X",()=>{
     let result = roman(10)
     expect(result).toBe("X")
 })
 
+test("given 40 should return XL",()=>{
+    let result = roman(40)
+    expect(result).toBe("XL")
+})
+
 test("given 50 should return L",()=>{
     let result = roman(50)
     expect(result).toBe("L")
+})
+
+test("given 90 should return XC",()=>{
+    let result = roman(90)
+    expect(result).toBe("XC")
 })
 
 test("given 100 should return C",()=>{
--- src/Roman.js
+++ src/Roman.js
@@ -1,11 +1,13 @@
 function roman(arabic) {
     var result = "I";
-    var arabics= [1,2,5,10,50,100]
-    var romans= ["I","II","V","X","L","C"]
+    // var arabics= [1,2,5,10,50,100]
+    var arabics= [1,4,5,9,10,40,50,90,100]
+    // var romans= ["I","II","V","X","L","C"]
+    var romans= ["I","IV","V","IX","X","XL","L","XC","C"]
+    
     arabics.forEach(function(item,index){
         if(arabic === item)
             result = romans[index];
-
     })
     return result;
 }
