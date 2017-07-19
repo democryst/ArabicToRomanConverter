c0176c4d60b46bb7148f2e0f2c3f0dbccf7bd4f5
Pawit Manussirivitaya <democryst@gmail.com>
2017-07-19 16:53:14 +0700

    remove size -> arabic.length

--- src/Roman.js
+++ src/Roman.js
@@ -12,9 +12,8 @@ function roman(arabic) {
         90: "XC",
         100: "C"
     }
-    var size = arabics.length;
     while (arabic > 0) {
-        for (let i = size - 1; i >= 0; i--) {
+        for (let i = arabics.length - 1; i >= 0; i--) {
             if (arabic >= arabics[i]) {
                 arabic -= arabics[i];
                 result += map[arabics[i]];
